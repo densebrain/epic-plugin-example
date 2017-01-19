acd380a339b0158a8f2d08646118c49983ac4717
jglanz <jonathan.glanz@gmail.com>
2017-01-18 22:17:10 -0500

    test/dev/plugin

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-export * from "./epic-plugin-example"
+
+export * from "./EpicSamplePlugin"
+
+import EpicPlugin,{Electron,getIssueActions} from 'epic-plugin-env'
+
+CommandRegistryScope.Register({
+	id: 'TestPluginCommand',
+	type: CommandType.App,
+	name: "Test Plugin Command",
+	defaultAccelerator: "CommandOrControl+n",
+	execute: (cmd, event) => {
+		const
+			{remote} = Electron,
+			{dialog,getCurrentWindow} = remote
+		
+		dialog.showMessageBox(getCurrentWindow(),{type: 'info',message: 'test dialog',buttons: ['ok']})
+	}
+})
